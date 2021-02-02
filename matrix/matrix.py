e5733eae8917eb403d900cf80eabb59e91fae5cc
sauravjain2304 <jainsaurav2304@gmail.com>
2021-02-02 15:52:09 +0530

    add program to print selected row or column of matrix

---
+++ matrix/matrix.py
@@ -0,0 +1,26 @@
+class Matrix:
+  def __init__(self, matrix_string):
+    self.a = matrix_string
+
+  def row(self, index):
+    nums = self.a.split("\n")
+    return list(map(int, nums[index - 1].split()))      
+    # print(f"{nums}")
+    # n = int(len(nums) ** 0.5)
+    # return list(map(list, zip(*[map(int, nums)] * n)))
+     
+  def column(self, index):
+    nums = self.a.split("\n")
+    col = []
+    for i in nums:
+      col.append(i.split()[index - 1])
+    # print(" ".join(map(str, col)))
+    return list(map(int, col))
+
+
+# m = Matrix("9 8 7\n5 3 2\n6 6 7")
+# for i in [1, 2, 3]:
+#   print(m.row(i))
+# print(m.column(1))
+
+# c = Matrix()
