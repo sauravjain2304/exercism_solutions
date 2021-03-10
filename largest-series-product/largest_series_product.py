448b4f4cd4b3be6c753df422a1f6d125bf5b2370
sauravjain2304 <jainsaurav2304@gmail.com>
2021-03-10 16:55:06 +0530

    add largest-series-product files

---
+++ largest-series-product/largest_series_product.py
@@ -0,0 +1,14 @@
+def largest_product(series, size):
+  if len(series) < size:
+    raise ValueError("error")
+  if size < 0:
+    raise ValueError("error")
+  largest = 0
+  for i in range(len(series) - size + 1):
+    t = 1
+    for j in range(size):
+      t = t * int(series[i + j])
+    if t > largest:
+      largest = t
+  return largest      
+print(largest_product("1027839564", 3))  
