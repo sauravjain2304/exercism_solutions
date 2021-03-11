64e8c84e4fd5c51e4dbc14fa53861e7ed5b8cd2e
sauravjain2304 <jainsaurav2304@gmail.com>
2021-03-11 16:53:06 +0530

    add etl files

---
+++ etl/etl.py
@@ -0,0 +1,7 @@
+def transform(legacy_data):
+  result = {}
+  for key in legacy_data:
+    for value in legacy_data[key]:
+      result[value.lower()] = key
+  return result
+print(transform({1: ["A", "E", "I", "O", "U"]}))  
