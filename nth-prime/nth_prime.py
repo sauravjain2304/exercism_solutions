b0a58cf986d4fbb01f596779ee3e83ec838bc932
sauravjain2304 <jainsaurav2304@gmail.com>
2021-03-22 13:44:50 +0530

    add nth prime files

--- nth-prime/nth_prime.py
+++ nth-prime/nth_prime.py
@@ -1,2 +1,15 @@
 def prime(number):
-  pass
+  i = 3
+  prime_nums = [2]
+  if number <= 0:
+    raise ValueError("error")
+  while True:
+    if len(prime_nums) == number:
+      return prime_nums[-1]
+    for j in range(2,i):
+      if i % j == 0:
+        break
+      if j == i-1:
+        prime_nums.append(i)
+    i += 1
+print(prime(20))    
