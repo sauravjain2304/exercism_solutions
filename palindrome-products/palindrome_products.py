3f196325bb00dbf5248e2fa77c6870482dbf74bb
sauravjain2304 <jainsaurav2304@gmail.com>
2021-04-01 22:20:33 +0530

    add palindrome-products files

--- palindrome-products/palindrome_products.py
+++ palindrome-products/palindrome_products.py
@@ -1,5 +1,43 @@
 def largest(min_factor, max_factor):
-  
+  if min_factor > max_factor:
+    raise ValueError("error")
+  product = 0
+  factors = []
+  for x in range(max_factor, min_factor-1, -1):
+    for y in range(x, min_factor-1, -1):
+      p = x * y
+      if p < product:
+        break
+      elif p == product and factors:
+        factors.append((x, y))
+        break
+      elif p > product and str(p) == str(p)[::-1]:
+        product, factors = p, [(x, y)]
+        break
+  if factors == []:
+    return None, []
+  else:
+    return product, factors
+print(largest(1, 9))    
 
 def smallest(min_factor, max_factor):
-  pass
+  if min_factor > max_factor:
+    raise ValueError("error")
+  product = max_factor ** 2
+  factors = []
+  for x in range(min_factor, max_factor+1):
+    for y in range(x, max_factor+1):
+      p = x * y
+      if p > product:
+        break
+      elif p == product and factors:
+        factors.append((x, y))
+        break
+      elif p < product and str(p) == str(p)[::-1]:
+        product, factors = p, [(x, y)]
+        break
+  if factors == []:
+    return None, []
+  else:
+    return product, factors
+print(smallest(10, 99))    
--- palindrome-products/palindrome_products_test.py
+++ palindrome-products/palindrome_products_test.py
@@ -1,3 +1,4 @@
+
 import unittest
 
 from palindrome_products import largest, smallest
