fa70763518f2343aed131ca80ef4bf86be36f202
sauravjain2304 <jainsaurav2304@gmail.com>
2021-04-12 13:25:07 +0530

    add robot-name files

---
+++ robot-name/robot_name.py
@@ -0,0 +1,14 @@
+from string import ascii_uppercase, digits
+import random
+class Robot:
+  def __init__(self):
+    self.name = self.generate_name()
+
+  def reset(self):
+    self.__init__()
+
+  def generate_name(self):
+    random.seed()
+    uppercase_ = random.choices(ascii_uppercase, k=2)
+    digits_ = random.choices(digits, k=3)
+    return ''.join(uppercase_ + digits_)
