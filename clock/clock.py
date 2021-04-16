0544df7b0336691a96c2fa3efb07abb0ac86391c
sauravjain2304 <jainsaurav2304@gmail.com>
2021-04-16 13:41:51 +0530

    add clock files

--- clock/clock.py
+++ clock/clock.py
@@ -1,16 +1,16 @@
 class Clock:
-    def __init__(self, hour, minute):
-      self.hour = hour
-      self.minute = minute
+  def __init__(self, hour, minute):
+    self.hour = (hour + minute//60) % 24
+    self.minute = minute % 60
 
-    def __repr__(self):
-        pass
+  def __repr__(self):
+    return f"{self.hour:02d}:{self.minute:02d}"
 
-    def __eq__(self, other):
-        pass
+  def __eq__(self, other):
+    return str(self) == str(other)
 
-    def __add__(self, minutes):
-        pass
+  def __add__(self, minutes):
+    return Clock(self.hour, self.minute + minutes)
 
-    def __sub__(self, minutes):
-        pass
+  def __sub__(self, minutes):
+    return Clock(self.hour, self.minute - minutes)
--- sum-of-multiples/sum_of_multiples.py
+++ sum-of-multiples/sum_of_multiples.py
@@ -1,2 +1,9 @@
 def sum_of_multiples(limit, multiples):
-    pass
+  factors = []    
+  for n in range(1,limit):
+    for m in multiples:
+      val = n*m
+      if val < limit and not val in factors:
+        factors.append(val)
+  return sum(factors)
+print(sum_of_multiples(1000, [3, 5]))
