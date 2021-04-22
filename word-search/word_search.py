26ed17ef682fc4f2e245304c40da2d93378eedaf
sauravjain2304 <jainsaurav2304@gmail.com>
2021-04-22 15:26:01 +0530

    add word-search files

--- word-search/word_search.py
+++ word-search/word_search.py
@@ -1,15 +1,34 @@
 class Point:
-    def __init__(self, x, y):
-        self.x = None
-        self.y = None
+  def __init__(self, x, y):
+    self.x = None
+    self.y = None
 
-    def __eq__(self, other):
-        return self.x == other.x and self.y == other.y
+  def __eq__(self, other):
+    return self.x == other.x and self.y == other.y
 
 
 class WordSearch:
-    def __init__(self, puzzle):
-        pass
+  directions = [(1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (1, -1), (-1, 1), (-1, -1)]
+  def __init__(self, puzzle):
+    self.puzzle = puzzle
 
-    def search(self, word):
-        pass
+  def search(self, word):
+    for y in range(len(self.puzzle)):
+      for x in range((len(self.puzzle[0]))):
+        if self.puzzle[y][x] != word[0]:
+          continue
+        else:
+          for d in self.directions:
+            chunk = self.puzzle[y][x]
+            x2 = x
+            y2 = y
+            while True:
+              try:
+                if chunk == word:
+                  return Point(x, y), Point(x2, y2)
+                x2 += d[0]
+                y2 += d[1]
+                chunk += self.puzzle[y2][x2]
+              except:
+                break
+  
